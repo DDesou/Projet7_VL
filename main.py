c750269938a8e4429a8780bd449d7412cb57ebf5
DDesou <140590021+DDesou@users.noreply.github.com>
2023-11-23 11:13:40 +0100

    adding model+update requirements

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  Body
+import pandas as pd
+import joblib
 
 app = FastAPI()
+joblib_in = open("model.joblib","rb")
+pipeline=joblib.load(joblib_in)
 
 @app.get("/")
 def read_root():
     return {"Bonjour": "Finally working!!!"}
+
+@app.get('/prediction/')
+def get_prediction(json_client: dict = Body({})):
+    """
+    Calculates the probability of default for a client.  
+    Args:  
+    - client data (json).  
+    Returns:    
+    - probability of default (dict).
+    """
+    df_one_client = pd.Series(json_client).to_frame().transpose()
+    probability = pipeline[1].predict_proba(df_one_client)[:, 1][0]
+    return {'probability': probability}
